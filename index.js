f15e272385e474e8332c8b1ef5d23dd33db678fc
Pritesh Jogdhankar <129420569+pritesh299@users.noreply.github.com>
2024-03-10 06:02:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,58 @@
-// javascript
-
-
+// Define a weather object with properties and methods
 let weather = {
-    apiKey: "aa17a7679cee6abc0aa939ebd0267518",
+    apiKey: "aa17a7679cee6abc0aa939ebd0267518", 
+
+
+    // Method to fetch weather data from API for a given city
     fetchWeather: (city) => {
+        // Fetch weather data from OpenWeatherMap API
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + weather.apiKey)
-            .then((response) => response.json())
-            .then((data) => weather.displayWeather(data))
+            .then((response) => response.json()) 
+            .then((data) => weather.displayWeather(data)) 
     },
+
+
+
+    // Method to display weather data on the webpage
     displayWeather: (data) => {
-        const { name } = data;
-        const { icon, description } = data.weather[0];
+        // Extract relevant data from the response
+        const { name } = data; 
+        const { icon, description } = data.weather[0]; 
         const { speed } = data.wind;
-        const { temp, humidity } = data.main;
+        const { temp, humidity } = data.main; 
+
+        // Update DOM elements with weather data
         document.querySelector(".city").innerText = name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/w/" + icon + ".png";
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".description").innerText = description;
         document.querySelector(".humidity").innerText = "Humidity:" + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed:" + speed + "Km/Hr";
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + description +"')"
         document.querySelector(".weather").classList.remove("loading")
     },
+
+
+    // Method to initiate weather search
+
     search: () => {
-        let searchText = document.querySelector(".searchBar")
-        weather.fetchWeather(searchText.value)
-        searchText.value = ""
-        console.log(searchText.value)
+        let searchText = document.querySelector(".searchBar") 
+        weather.fetchWeather(searchText.value) 
+        searchText.value = "";
     }
 }
 
+// Event listener for pressing Enter key in the search input
 document.querySelector(".searchBar").addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
-        weather.search();
-        
+        weather.search(); 
     }
 })
+
+// Event listener for clicking the search button
 document.querySelector(".search button").addEventListener("click", () => {
-    weather.search()
+    weather.search(); 
 })
+
+// Fetch weather data for New York when the page loads
 weather.fetchWeather("new york")
